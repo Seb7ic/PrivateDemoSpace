ecac435be2b871ccd622b78e1e2c05991a70f217
Seb7ic <sstork@SEBNo2MCBK0046.ads.eb.intern>
2018-10-11 16:25:01 +0200

    thanks

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,9 +7,13 @@ public class HelloWorld {
 		
 		MessagePrinter ownMsgPrntr = new MessagePrinter("I'm awesome!");
 		ownMsgPrntr.printMessage();
+		
+		MessagePrinter anotherMsgPrntr = new MessagePrinter("I'm even better!");
+		anotherMsgPrntr.printMessage();
 	}
 
 }
 
 
 
+ 
